23d76a549c91efb566985bd7740b9eab92eb75a9
waybackarchiver <66856220+waybackarchiver@users.noreply.github.com>
2021-08-06 00:12:20 +0000

    Using test configuration

--- examples/auth/main.go
+++ examples/auth/main.go
@@ -34,7 +34,7 @@ func main() {
 		// where to store session configuration. must be set
 		SessionFile: sessionFile,
 		// host address of mtproto server. Actually, it can be any mtproxy, not only official
-		ServerHost: "149.154.167.50:443",
+		ServerHost: "149.154.167.40:443",
 		// public keys file is path to file with public keys, which you must get from https://my.telegram.org
 		PublicKeysFile:  publicKeys,
 		AppID:           94575,                              // app id, could be find at https://my.telegram.org
