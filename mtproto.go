40c842f3e4d3d99281fd29651d5e6de7fbc16f67
Amarnath C <amarnathcharichilil@gmail.com>
2022-10-21 19:14:02 +0530

    Update mtproto.go

--- mtproto.go
+++ mtproto.go
@@ -53,6 +53,8 @@ type MTProto struct {
 	// идентификаторы сообщений, нужны что бы посылать и принимать сообщения.
 	seqNoMutex sync.Mutex
 	seqNo      int32
+	lastMessageIDMutex sync.Mutex
+	lastMessageID      int64
 
 	// айдишники DC для КОНКРЕТНОГО Приложения и клиента. Может меняться, но фиксирована для
 	// связки приложение+клиент
